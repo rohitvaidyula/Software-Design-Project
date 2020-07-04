b5668cb16fb5186a3150f81c90d6d99335e6f097
altub <60374767+altub@users.noreply.github.com>
2020-07-04 15:17:55 -0700

    Add files via upload

---
+++ Router Code/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+//import pages from saved location
+import ProfilePage from './Profile page/Profile page';
+import FuelQuoteHistory from './Fuel Quote Hist/histPage';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  //set path in <Route/>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/FuelQuoteHist" component={FuelQuoteHistory}/>
+      <Route exact path="/ProfilePage" component={ProfilePage}/>
+    </Switch>
+  </BrowserRouter>,
+  rootElement
+);
