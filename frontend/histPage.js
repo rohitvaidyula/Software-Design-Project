c05845adff455a13de19f677befc10ae25dc690c
altub <60374767+altub@users.noreply.github.com>
2020-07-12 15:04:24 -0700

    Add files via upload

--- frontend/histPage.js
+++ frontend/histPage.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './histPage.css';
+import { render } from 'react-dom';
+import {Link, BrowserRouter, Route, Router} from 'react-router-dom';
+import Form from '../Profile page/Profile page'
+
+
 
 class hist extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {items: []};
+  }
+  componentDidMount(){
+    fetch("http://localhost:5000/api/test/info")
+      .then(res=>res.json())
+      .then(data=>{
+        this.setState({
+          items: data
+        })
+      })
+      // .then(res=>res.text())
+      // .then(res=>this.setState({apiResponse: res}));
+  }
+
   render(){
+    
     return(
       <body>
       <header>
       <div className="pageButtons">
-        <button className="profilePage">
-          Profile
-        </button>
+      <Link to='/ProfilePage'><button className="profilePage">Profile</button></Link>
         <button className="fuelQuotePage">
           Fuel Quote
         </button>
+        
       </div>
     </header>
 
@@ -30,11 +51,20 @@ class hist extends React.Component{
         Quote ID
       </label>
     </div>
+
+      <div className="labels">
+        <label className="username">{this.state.items.map(item=><p>{item.username}</p>)}</label>
+        <label className="date">{this.state.items.map(item=><p>{item.date}</p>)}</label>
+        <label className="finalPrice">{this.state.items.map(item=><p>{item.finalPrice}</p>)}</label>
+        <label className="quoteID">{this.state.items.map(item=><p>{item.quoteID}</p>)}</label>
+      </div>
     
   </body>
     );
   }
 }
+//using username currently may want clientID
+//<label className="clientID">{this.state.items.map(item=><p>{item.clientID}</p>)}</label>
 
 
 export default hist;
