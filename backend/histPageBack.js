60cc79cfe1fd7c8b522520d3a777cf9ab063c09c
altub <60374767+altub@users.noreply.github.com>
2020-07-12 15:03:19 -0700

    Add files via upload

--- backend/histPageBack.js
+++ backend/histPageBack.js
@@ -1,11 +1,48 @@
 const {Router} = require('express');
-const fakeDat = require('../testData/testData.json')
+const fakeDat = require('../testData/testData.json');
+const mongoose = require('mongoose');
+
+
+
 
 const router = Router();
 
+const historySchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+        minLenght:5
+    },
+    //added final price in case may want/need final price
+    finalPrice:{
+        type:Number
+    },
+    quoteID:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+},{collection:'dat'},
+    {
+        timestamps:true
+});
+
+const historyModel = mongoose.model('dat',historySchema);
+
+
+
 router.get('/info',async (req,res)=>{
     try{
-        res.status(200).json(fakeDat)
+        //console.log(historyModel.find())
+        //search for all history with a certain username/clientID
+        historyModel.find({username: "first"},function(err,data){
+            res.json(data)
+        });
+        //res.status(200).json(fakeDat)
     }
     catch(error){
         res.status(500).json({message:'Server error ', error});
